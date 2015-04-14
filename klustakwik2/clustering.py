3ec6afe9e4e9137073b04a47bf136e0d24e8f2c2
thesamovar <dg.github@thesamovar.net>
2015-04-14 20:40:36 +0100

    Added subsetting and default parameters as a dict (could be moved somewhere else)

--- klustakwik2/clustering.py
+++ klustakwik2/clustering.py
@@ -21,37 +21,33 @@ def get_diagonal(x):
     '''
     return x.reshape(-1)[::x.shape[0]+1]
 
+default_parameters = dict(
+     prior_point=1,
+     mua_point=1,
+     noise_point=1,
+     points_for_cluster_mask=10,
+     dist_thresh=log(1000),
+     penalty_k=0.0,
+     penalty_k_log_n=1.0,
+     max_iterations=10000,
+     num_changed_threshold=0.05,
+     full_step_every=20,
+     split_first=20,
+     split_every=40,
+     )                          
 
 class KK(object):
-    def __init__(self, data,
-                 prior_point=1,
-                 mua_point=1,
-                 noise_point=1,
-                 points_for_cluster_mask=10,
-                 dist_thresh=log(1000),
-                 penalty_k=0.0,
-                 penalty_k_log_n=1.0,
-                 max_iterations=10000,
-                 num_changed_threshold=0.05,
-                 full_step_every=20,
-                 split_first=20,
-                 split_every=40,
-                 ):
-        
+    def __init__(self, data, **params):        
         self.data = data
+        self.params = params
+        actual_params = default_parameters.copy()
+        actual_params.update(**params)
+        for k, v in actual_params.iteritems():
+            setattr(self, k, v)
         
-        self.prior_point = prior_point
-        self.mua_point = mua_point
-        self.noise_point = noise_point
-        self.points_for_cluster_mask = points_for_cluster_mask
-        self.dist_thresh = dist_thresh
-        self.penalty_k = penalty_k
-        self.penalty_k_log_n = penalty_k_log_n
-        self.max_iterations = max_iterations
-        self.num_changed_threshold = num_changed_threshold
-        self.full_step_every = full_step_every
-        self.split_first = split_first
-        self.split_every = split_every
+    def subset(self, spikes):
+        newdata = self.data.subset(spikes)
+        return KK(newdata, **self.params)
     
     def cluster(self, num_starting_clusters):
         self.clusters = mask_starts(self.data, num_starting_clusters)
@@ -95,6 +91,7 @@ class KK(object):
                     (self.current_iteration>self.split_first or
                      self.current_iteration-self.split_first%self.split_every==self.split_every-1 or
                      (num_changed==0 and last_step_full))):
+                    
                     did_split = self.try_splits()
                     
             if num_changed==0 and last_step_full and not did_split:
@@ -324,4 +321,156 @@ class KK(object):
             self.covariance.append(BlockPlusDiagonalMatrix(masked, unmasked))
             
     def try_splits(self):
-        return False
+        did_split = False
+        num_clusters = len(self.num_cluster_members)
+        
+        if num_clusters>=self.max_possible_clusters-2:
+            print "Won't try splitting - already at maximum number of clusters"
+            return False
+        
+#     if (!AlwaysSplitBimodal)
+#     {
+#         if (KK_split == NULL)
+#         {
+#             KK_split = new KK(*this);
+#         }
+#         else
+#         {
+#             // We have to clear these to bypass the debugging checks
+#             // in precomputations.cpp
+#             KK_split->Unmasked.clear();
+#             KK_split->UnmaskedInd.clear();
+#             KK_split->SortedMaskChange.clear();
+#             KK_split->SortedIndices.clear();
+#             // now we treat it as empty
+#             KK_split->ConstructFrom(*this);
+#         }
+#     }
+#     //KK &K3 = *KK_split;
+# #define K3 (*KK_split)
+
+        score = self.compute_score()
+        
+        for cluster in xrange(2, num_clusters):
+            pass
+# 
+#         // set up K2 structure to contain points of this cluster only
+# 
+#         vector<integer> SubsetIndices;
+#         for(p=0; p<nPoints; p++)
+#             if(Class[p]==c)
+#                 SubsetIndices.push_back(p);
+#         if(SubsetIndices.size()==0)
+#             continue;
+# 
+#         if (K2_container)
+#         {
+#             // We have to clear these to bypass the debugging checks
+#             // in precomputations.cpp
+#             K2_container->Unmasked.clear();
+#             K2_container->UnmaskedInd.clear();
+#             K2_container->SortedMaskChange.clear();
+#             K2_container->SortedIndices.clear();
+#             //K2_container->AllVector2Mean.clear();
+#             // now we treat it as empty
+#             K2_container->ConstructFrom(*this, SubsetIndices);
+#         }
+#         else
+#         {
+#             K2_container = new KK(*this, SubsetIndices);
+#         }
+#         //KK K2(*this, SubsetIndices);
+#         KK &K2 = *K2_container;
+# 
+#         // find an unused cluster
+#         UnusedCluster = -1;
+#         for(c2=2; c2<MaxPossibleClusters; c2++)
+#         {
+#              if (!ClassAlive[c2])
+#              {
+#                  UnusedCluster = c2;
+#                  break;
+#              }
+#         }
+#         if (UnusedCluster==-1)
+#         {
+#             Output("No free clusters, abandoning split");
+#             return DidSplit;
+#         }
+# 
+#         // do it
+#         if (Verbose >= 1) Output("\n Trying to split cluster %d (%d points) \n", (int)c, (int)K2.nPoints);
+#         K2.nStartingClusters=3; // (3 = 1 clusters + 2 unused noise/MUA cluster)
+#         UnsplitScore = K2.CEM(NULL, 0, 1, false);
+#         K2.nStartingClusters=4; // (4 = 2 clusters + 2 unused noise/MUA cluster)
+#         SplitScore = K2.CEM(NULL, 0, 1, false);
+# 
+#         // Fix by Michael Zugaro: replace next line with following two lines
+#         // if(SplitScore<UnsplitScore) {
+#         if(K2.nClustersAlive<3) Output("\n Split failed - leaving alone\n");
+#         if((SplitScore<UnsplitScore)&&(K2.nClustersAlive>=3)) {
+#             if (AlwaysSplitBimodal)
+#             {
+#                 DidSplit = 1;
+#                 Output("\n We are always splitting bimodal clusters so it's getting split into cluster %d.\n", (int)UnusedCluster);
+#                 p2 = 0;
+#                 for (p = 0; p < nPoints; p++)
+#                 {
+#                     if (Class[p] == c)
+#                     {
+#                         if (K2.Class[p2] == 2) Class[p] = c;
+#                         else if (K2.Class[p2] == 3) Class[p] = UnusedCluster;
+#                         else Error("split should only produce 2 clusters\n");
+#                         p2++;
+#                     }
+#                     ClassAlive[Class[p]] = 1;
+#                 }
+#             }
+#             else
+#             {
+#                 // will splitting improve the score in the whole data set?
+# 
+#                 // assign clusters to K3
+#                 for (c2 = 0; c2 < MaxPossibleClusters; c2++) K3.ClassAlive[c2] = 0;
+#                 //   Output("%d Points in class %d in KKobject K3 ", (int)c2, (int)K3.nClassMembers[c2]);
+#                 p2 = 0;
+#                 for (p = 0; p < nPoints; p++)
+#                 {
+#                     if (Class[p] == c)
+#                     {
+#                         if (K2.Class[p2] == 2) K3.Class[p] = c;
+#                         else if (K2.Class[p2] == 3) K3.Class[p] = UnusedCluster;
+#                         else Error("split should only produce 2 clusters\n");
+#                         p2++;
+#                     }
+#                     else K3.Class[p] = Class[p];
+#                     K3.ClassAlive[K3.Class[p]] = 1;
+#                 }
+#                 K3.Reindex();
+# 
+#                 // compute scores
+# 
+#                 K3.MStep();
+#                 K3.EStep();
+#                 //Output("About to compute K3 class penalties");
+#                 if (UseDistributional) K3.ComputeClassPenalties(); //SNK Fixed bug: Need to compute the cluster penalty properly, cluster penalty is only used in UseDistributional mode
+#                 NewScore = K3.ComputeScore();
+#                 Output("\nSplitting cluster %d changes total score from " SCALARFMT " to " SCALARFMT "\n", (int)c, Score, NewScore);
+# 
+#                 if (NewScore < Score)
+#                 {
+#                     DidSplit = 1;
+#                     Output("\n So it's getting split into cluster %d.\n", (int)UnusedCluster);
+#                     // so put clusters from K3 back into main KK struct (K1)
+#                     for (c2 = 0; c2 < MaxPossibleClusters; c2++) ClassAlive[c2] = K3.ClassAlive[c2];
+#                     for (p = 0; p < nPoints; p++) Class[p] = K3.Class[p];
+#                 }
+#                 else
+#                 {
+#                     Output("\n So it's not getting split.\n");
+#                 }
+#             }
+#         }
+#     }
+#     return DidSplit;
+# #undef K3
