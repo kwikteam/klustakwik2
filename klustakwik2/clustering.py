fb0bb5acabf26cea1262599f5c242429b356da79
thesamovar <dg.github@thesamovar.net>
2015-04-14 20:23:04 +0100

    Use numba for part of M-step: seems to be good

--- klustakwik2/clustering.py
+++ klustakwik2/clustering.py
@@ -6,7 +6,9 @@ from .mask_starts import mask_starts
 from .linear_algebra import BlockPlusDiagonalMatrix
 
 from .cylib.compute_cluster_masks import accumulate_cluster_mask_sum
-from .cylib.m_step import compute_cluster_means, compute_covariance_matrix
+# from .cylib.m_step import compute_cluster_means, compute_covariance_matrix
+from .cylib.m_step import compute_covariance_matrix
+from .numbalib.m_step import compute_cluster_means
 from .cylib.e_step import compute_log_p_and_assign
 
 import time
@@ -52,13 +54,9 @@ class KK(object):
         self.split_every = split_every
     
     def cluster(self, num_starting_clusters):
-        start = time.time()
         self.clusters = mask_starts(self.data, num_starting_clusters)
         self.old_clusters = -1*ones(len(self.clusters), dtype=int)
-        print 'Mask starts:', time.time()-start
-        start = time.time()
         self.reindex_clusters()
-        print 'Reindex clusters:', time.time()-start
         self.CEM()
     
     def CEM(self, recurse=True):
@@ -70,22 +68,12 @@ class KK(object):
         
         while self.current_iteration==0: # for debugging
         #while self.current_iteration<=self.max_iterations:
-            start = time.time()
             self.M_step()
-            print 'M_step:', time.time()-start
-            start = time.time()
             self.EC_steps()
-            print 'EC_steps:', time.time()-start
-            start = time.time()
             self.compute_cluster_penalties()
-            print 'compute_cluster_penalties:', time.time()-start
             if recurse:
-                start = time.time()
                 self.consider_deletion()
-                print 'consider_deletion:', time.time()-start
-            start = time.time()
             old_score, score = score, self.compute_score()
-            print 'compute_score:', time.time()-start
             
             num_changed = sum(self.clusters!=self.old_clusters)
             
@@ -116,10 +104,8 @@ class KK(object):
         # eliminate any clusters with 0 members, compute the list of spikes
         # in each cluster, compute the cluster masks and allocate space for
         # covariance matrices
-        start = time.time()
         self.reindex_clusters()
         self.compute_cluster_masks()
-        print 'compute_cluster_masks:', time.time()-start  
 
         num_cluster_members = self.num_cluster_members
         num_clusters = len(self.num_cluster_members)
@@ -136,12 +122,12 @@ class KK(object):
         # Compute means for each cluster
         # Note that we do this densely at the moment, might want to switch
         # that to a sparse structure later
+        compute_cluster_means(self)
         start = time.time()
         self.cluster_mean = compute_cluster_means(self)
-        print 'Compute cluster_mean:', time.time()-start  
-                
+        print time.time()-start
+        
         # Compute covariance matrices
-        start = time.time()
         for cluster in xrange(2, num_clusters):
             compute_covariance_matrix(self, cluster)
             cov = self.covariance[cluster]
@@ -155,8 +141,6 @@ class KK(object):
             factor = 1.0/(num_cluster_members[cluster]+self.prior_point-1)
             cov.block *= factor
             cov.diagonal *= factor
-                        
-        print 'Compute covariance matrices:', time.time()-start
                     
     def EC_steps(self, allow_assign_to_noise=True):
         if not allow_assign_to_noise:
---
+++ klustakwik2/numbalib/m_step.py
@@ -0,0 +1,52 @@
+import numpy
+import numba
+
+def compute_cluster_means(kk):
+    num_clusters = len(kk.num_cluster_members)
+    num_features = kk.num_features
+    data = kk.data
+
+    cluster_mean = numpy.zeros((num_clusters, num_features))
+    num_added = numpy.zeros((num_clusters, num_features), dtype=int)
+
+    _compute_cluster_means(kk.clusters,
+                           data.unmasked, data.unmasked_start, data.unmasked_end,
+                           data.features, data.values_start, data.values_end,
+                           cluster_mean, num_added,
+                           kk.mua_point, kk.prior_point,
+                           data.noise_mean, kk.num_cluster_members)
+                    
+    return cluster_mean
+
+@numba.jit(nopython=True)
+def _compute_cluster_means(
+            clusters,
+            unmasked, ustart, uend,
+            features, vstart, vend,
+            cluster_mean, num_added,
+            mua_point, prior_point,
+            noise_mean, num_cluster_members,
+            ):
+    
+    num_clusters = len(num_cluster_members)
+    num_features = cluster_mean.shape[1]
+    
+    for p in xrange(len(clusters)):
+        c = clusters[p]
+        num_unmasked = uend[p]-ustart[p]
+        for i in xrange(num_unmasked):
+            j = unmasked[ustart[p]+i]
+            k = vstart[p]+i
+            cluster_mean[c, j] += features[k]
+            num_added[c, j] += 1
+
+    for cluster in xrange(num_clusters):
+        prior = 0
+        if cluster==1:
+            prior = mua_point
+        elif cluster>=2:
+            prior = prior_point
+        for i in xrange(num_features):
+            cluster_mean[cluster, i] += noise_mean[i]*(num_cluster_members[cluster]-num_added[cluster, i]) 
+            cluster_mean[cluster, i] += prior*noise_mean[i]
+            cluster_mean[cluster, i] /= num_cluster_members[cluster]+prior
