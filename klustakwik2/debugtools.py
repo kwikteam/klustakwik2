daf53e26477ebde771de7ef0193a73191bbf176e
thesamovar <dan.goodman@ens.fr>
2015-04-30 03:41:06 +0100

    Added dump_cluster_count debug tool

--- klustakwik2/debugtools.py
+++ klustakwik2/debugtools.py
@@ -2,7 +2,7 @@
 Some tools for debugging
 '''
 
-__all__ = ['dump_covariance_matrices']
+__all__ = ['dump_covariance_matrices', 'dump_cluster_counts']
 
 def covariance_matrix_dump_callback(kk):
     for cluster, cov in enumerate(kk.covariance):
@@ -11,5 +11,14 @@ def covariance_matrix_dump_callback(kk):
         msg += 'Block:\n%s\nDiagonal:\n%s\n' % (cov.block, cov.diagonal)
         kk.log('debug', msg, suffix='covariance_matrices')
 
+
 def dump_covariance_matrices(kk):
     kk.register_callback(covariance_matrix_dump_callback, 'end_M_step')
+
+
+def cluster_count_dump_callback(kk):
+    kk.log('debug', 'Cluster counts: %s' % kk.num_cluster_members, suffix='cluster_counts')
+
+
+def dump_cluster_counts(kk, slot='end_EC_steps'):
+    kk.register_callback(cluster_count_dump_callback, slot=slot)
