6b777d6fea8368c37a24d5b8efcd0737ee007cfe
thesamovar <dg.github@thesamovar.net>
2015-06-03 23:38:42 +0100

    So close...

--- klustakwik2/tests/__init__.py
+++ klustakwik2/tests/__init__.py
@@ -7,12 +7,10 @@ __all__ = ['run']
 def run():
     klustakwik2.console_log_level('critical')
     dirname = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-    nose.run(argv=['', dirname,
-                   '-c=',  # no config file loading
-                   '-I', '^\.',
-                   '-I', '^_',
-                   '--logging-clear-handlers',
-                    '--nologcapture',
-#                    '--exe',
-                   ])
-        
+    return nose.run(argv=['', dirname,
+                          '-c=',  # no config file loading
+                          '-I', '^\.',
+                          '-I', '^_',
+                          '--logging-clear-handlers',
+                          '--nologcapture',
+                          ])
