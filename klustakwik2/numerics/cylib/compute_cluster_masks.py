f90cd0883a9a9301f359c7a238aba223756c6765
thesamovar <dg.github@thesamovar.net>
2015-06-11 00:30:18 +0100

    Fix for some version of py64 on win64

--- klustakwik2/numerics/cylib/compute_cluster_masks.py
+++ klustakwik2/numerics/cylib/compute_cluster_masks.py
@@ -6,4 +6,4 @@ def accumulate_cluster_mask_sum(kk, cluster_mask_sum):
     data = kk.data
     doaccum(kk.clusters, data.unmasked, data.unmasked_start, data.unmasked_end,
             data.masks, data.values_start, data.values_end, cluster_mask_sum,
-            kk.num_special_clusters)
+            kk.clusters.dtype.type(kk.num_special_clusters))
