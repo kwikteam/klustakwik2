e5d376f6203413b0ebff2dbdd256988846ca3de1
thesamovar <dg.github@thesamovar.net>
2015-06-13 22:17:40 +0100

    Distribution using IPython.parallel: not highly efficient maybe, and try_splits is not distributed yet

--- klustakwik2/distributed.py
+++ klustakwik2/distributed.py
@@ -4,7 +4,9 @@ Wrapper for distributing work over multiple machines.
 This can be used to abstract distribution using multiprocessing, IPython, etc.
 '''
 
-__all__ = ['Distributer', 'MockDistributer']
+from copy import deepcopy
+
+__all__ = ['Distributer', 'MockDistributer', 'IPythonDistributer']
 
 class Distributer(object):
     '''
@@ -52,3 +54,51 @@ class MockDistributer(Distributer):
                 clusters_second_best=kk_copy.clusters_second_best,
                 )
             yield res
+
+
+class IPythonDistributer(Distributer):
+    '''
+    Class for distributed computation using IPython Client
+
+    TODO:
+    targets = client.targets # list of ints
+    client.push(namespace)
+    client.execute(codestr, targets=[list of ints])
+    x.pull('varname', block=True)
+    '''
+    def __init__(self, client):
+        self.client = client
+        self.dview = client[:]
+
+    def start(self, kk):
+        kk_copy = kk.copy_without_callbacks()
+        self.dview.push({'kk': kk_copy})
+
+    def iteration(self, clusters, cluster_order, full_step, only_evaluate_current_clusters):
+        self.dview.push({'clusters': clusters, 'full_step': full_step,
+                         'only_evaluate_current_clusters': only_evaluate_current_clusters})
+        self.dview.execute('''
+from copy import deepcopy
+kk = deepcopy(kk)
+kk.clusters = clusters
+kk.full_step = full_step
+kk.reindex_clusters()
+kk.prepare_for_MEC_steps(only_evaluate_current_clusters=only_evaluate_current_clusters)
+result = dict(
+    log_p_best=kk.log_p_best,
+    log_p_second_best=kk.log_p_second_best,
+    clusters=kk.clusters,
+    clusters_second_best=kk.clusters_second_best,
+    )
+''')
+        targets = self.dview.targets
+        N = len(targets)
+        for i, cluster in enumerate(cluster_order):
+            target = targets[i%N]
+            self.dview.execute(
+                'kk.MEC_steps_cluster(%d, only_evaluate_current_clusters=only_evaluate_current_clusters)' % cluster,
+                targets=[target])
+
+    def iteration_results(self):
+        for res in self.dview.pull('result'):
+            yield deepcopy(res)
