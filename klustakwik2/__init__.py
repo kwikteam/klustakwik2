ce3f5c95b52b78c88a6832480924eaf0bd207209
Cyrille Rossant <cyrille.rossant@gmail.com>
2016-03-21 18:03:01 +0100

    Bump version

--- klustakwik2/__init__.py
+++ klustakwik2/__init__.py
@@ -8,7 +8,7 @@ from .monitoring import *
 from .debugtools import *
 from .default_parameters import *
 
-__version__ = '0.2.4'
+__version__ = '0.2.5'
 __version__ = get_kk_version(__version__)
 
 log_message('info', 'KlustaKwik2 version '+__version__)
